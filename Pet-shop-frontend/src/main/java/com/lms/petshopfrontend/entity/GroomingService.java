a99b1ed1d73294ebe1d2ddbb999fdabc677f3e6b
irshadwnl <irshadwnl07@gmail.com>
2025-06-13 01:19:41 +0530

    [Irshad] added frontend grooming service entity

---
+++ Pet-shop-frontend/src/main/java/com/lms/petshopfrontend/entity/GroomingService.java
@@ -0,0 +1,34 @@
+package com.lms.petshopfrontend.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "grooming_services")
+public class GroomingService {
+    @Id
+    @Column(name = "service_id", nullable = false)
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "price", precision = 38, scale = 2)
+    private BigDecimal price;
+
+    @ColumnDefault("1")
+    @Column(name = "available")
+    private Boolean available;
+}
