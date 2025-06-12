83f9061f8947e46313135487c675714688d9d1cc
Dishank Bishnoi <90502552+dishankbishnoi@users.noreply.github.com>
2025-06-12 15:46:47 +0530

    Dishank Bishnoi added  frontend

---
+++ Pet-shop-frontend/src/main/java/com/lms/petshopfrontend/PetShopFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.lms.petshopfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetShopFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetShopFrontendApplication.class, args);
+	}
+
+}
