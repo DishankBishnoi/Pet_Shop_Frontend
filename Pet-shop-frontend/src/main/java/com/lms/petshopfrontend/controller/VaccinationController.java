ac4dc90a94be92be28af5ed5050eede0cb384501
Harsimran Kaur <harsimran0596.be21@chitkara.edu.in>
2025-06-13 01:21:41 +0530

    [Harsimran] add: Implement VaccinationController for listing, adding, and editing vaccinations

---
+++ Pet-shop-frontend/src/main/java/com/lms/petshopfrontend/controller/VaccinationController.java
@@ -0,0 +1,75 @@
+package org.example.frontend.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.example.frontend.dto.VaccinationDto;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/vaccinations")
+public class VaccinationController {
+
+    private final WebClient webClient = WebClient.create("http://localhost:8080/api/v1");
+
+    @GetMapping
+    public String getVaccinations(Model model) {
+        List<VaccinationDto> vaccinations = webClient.get()
+                .uri("/vaccinations")
+                .retrieve()
+                .bodyToFlux(VaccinationDto.class)
+                .collectList()
+                .block();
+
+        model.addAttribute("vaccinations", vaccinations);
+        return "vaccinations"; // maps to vaccinations.html
+    }
+
+    @GetMapping("/add")
+    public String addForm(Model model) {
+        model.addAttribute("vaccination", new VaccinationDto());
+        return "add-vaccination";
+    }
+//http://localhost:8080/api/v1/vaccinations/add
+
+    @PostMapping("/add")
+    public String addVaccination(@ModelAttribute VaccinationDto vaccination) {
+        webClient.post()
+                .uri("/vaccinations/add")
+                .body(Mono.just(vaccination), VaccinationDto.class)
+                .retrieve()
+                .bodyToMono(Void.class)
+                .block();
+
+        return "redirect:/vaccinations";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editForm(@PathVariable Integer id, Model model) {
+        VaccinationDto vaccination = webClient.get()
+                .uri("/vaccinations/{id}", id)
+                .retrieve()
+                .bodyToMono(VaccinationDto.class)
+                .block();
+
+        model.addAttribute("vaccination", vaccination);
+        return "edit-vaccination";
+    }
+
+    @PostMapping("/edit")
+    public String editVaccination(@ModelAttribute VaccinationDto vaccination) {
+        webClient.put()
+                .uri("/vaccinations/update/{id}", vaccination.getId())
+                .body(Mono.just(vaccination), VaccinationDto.class)
+                .retrieve()
+                .bodyToMono(Void.class)
+                .block();
+
+        return "redirect:/vaccinations";
+    }
+}
